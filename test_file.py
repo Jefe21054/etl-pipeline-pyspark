08b990039119bb6595e04d7459ebcb766b041fc0
Jefe21054 <ivaniglesias500@protonmail.ch>
2024-01-02 01:09:59 -0500

    Agregadas Pruebas Unitarias

--- test_file.py
+++ test_file.py
@@ -1,94 +1,37 @@
 # Import required libraries
+import os
 import pyspark.sql
 from pyspark.sql.functions import col
-from decouple import config
 
 # Create spark session
 spark = pyspark.sql.SparkSession \
     .builder \
-    .appName("Python Spark SQL basic example") \
+    .appName(" Testing Python Spark SQL ") \
     .config('spark.driver.extraClassPath', "driver/postgresql-42.2.18.jar") \
     .getOrCreate()
 
-# Read data from .env file with python-decouple
-DB_NAME = config('DB_NAME')
-DB_USER = config('DB_USER')
-DB_PASSWORD = config('DB_PASSWORD')
-DB_HOST = config('DB_HOST')
-DATABASE_PORT = config('DATABASE_PORT')
-POSTGRESQL_DRIVER = 'org.postgresql.Driver'
-
-url_conexion = f'jdbc:postgresql://{DB_HOST}:{DATABASE_PORT}/{DB_NAME}'
-usuario_conexion = f'{DB_USER}'
-clave_conexion = f'{DB_PASSWORD}'
+data = [("1", "4"), ("1", "5"), ("2", "3"), ("2", "4"), ("2", "5")]
+columns = ["movie_id", "rating"]
+users_df = spark.createDataFrame(data, columns)
 
 
-# Read movies table from db using Spark JDBC
-def extract_movies_to_df():
-    movies_df = spark.read \
-        .format("jdbc") \
-        .option("url", url_conexion) \
-        .option("dbtable", "movies") \
-        .option("user", usuario_conexion) \
-        .option("password", clave_conexion) \
-        .option("driver", POSTGRESQL_DRIVER) \
-        .load()
-    return movies_df
+def test_driver():
+    ''' Unit Test para verificar que 
+        existe la carpeta y el driver. '''
+    assert os.path.exists('driver')
 
 
-# Read users table from db using Spark JDBC
-def extract_users_to_df():
-    users_df = spark.read \
-        .format("jdbc") \
-        .option("url", url_conexion) \
-        .option("dbtable", "users") \
-        .option("user", usuario_conexion) \
-        .option("password", clave_conexion) \
-        .option("driver", POSTGRESQL_DRIVER) \
-        .load()
-    return users_df
-
-
-def transform_avg_ratings(movies_df, users_df):
-    # Transforming tables
-    new_users_df = users_df.withColumn("rating", col("rating").cast("int"))
-    avg_rating = new_users_df.groupBy("movies_id").mean("rating")
-    transform_df = movies_df.join(
-        avg_rating,
-        movies_df.id == avg_rating.movies_id
-    )
-    transform_df = transform_df.drop("movies_id")
-    return transform_df
-
-
-def test_datos(movies_df, users_df,transform_df):
+def test_datos(users_df):
     ''' Unit Test para las funciones que
         crean dataframes, el programa 
         debe arrojar siempre dataframes 
         que contengan datos. '''
 
-    df1 = movies_df
-    df2 = users_df
-    df3 = transform_df
-    
+    df = users_df
     MENSAJE_ERROR = 'The dataframe cannot be empty'
 
     # Verifying empty assertions
-    assert df1.count() > 0, MENSAJE_ERROR
-    assert df2.count() > 0, MENSAJE_ERROR
-    assert df3.count() > 0, MENSAJE_ERROR
+    assert df.count() > 0, MENSAJE_ERROR
 
-
-# Load transformed dataframe to the database
-def load_df_to_db(transform_df):
-    mode = 'overwrite'
-    properties = {
-                 "user": usuario_conexion,
-                 "password": clave_conexion,
-                 "driver": POSTGRESQL_DRIVER
-                 }
-    transform_df.write.jdbc(url=url_conexion,
-                            table="avg_ratings",
-                            mode=mode,
-                            properties=properties
-                            )
+# Stop the Spark session
+spark.stop()
