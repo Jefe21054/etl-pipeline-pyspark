3e6994d56900d8c7b791ea6d74be326f0238a7eb
Jefe21054 <ivaniglesias500@protonmail.ch>
2024-01-02 01:20:53 -0500

    Agregadas Pruebas Unitarias

--- test_file.py
+++ test_file.py
@@ -1,7 +1,6 @@
 # Import required libraries
 import os
 import pyspark.sql
-from pyspark.sql.functions import col
 
 # Create spark session
 spark = pyspark.sql.SparkSession \
@@ -10,28 +9,32 @@ spark = pyspark.sql.SparkSession \
     .config('spark.driver.extraClassPath', "driver/postgresql-42.2.18.jar") \
     .getOrCreate()
 
-data = [("1", "4"), ("1", "5"), ("2", "3"), ("2", "4"), ("2", "5")]
-columns = ["movie_id", "rating"]
-users_df = spark.createDataFrame(data, columns)
+sample_data = [{"movie_id": "1", "rating": 4},
+               {"movie_id": "1", "rating": 5},
+               {"movie_id": "2", "rating": 3},
+               {"movie_id": "2", "rating": 4},
+               {"movie_id": "2", "rating": 5}]
+users_df = spark.createDataFrame(sample_data)
 
 
 def test_driver():
-    ''' Unit Test para verificar que 
-        existe la carpeta y el driver. '''
+    '''Unit Test para verificar que
+        existe la carpeta y el driver.'''
     assert os.path.exists('driver')
 
 
 def test_datos(users_df):
-    ''' Unit Test para las funciones que
-        crean dataframes, el programa 
-        debe arrojar siempre dataframes 
-        que contengan datos. '''
+    '''Unit Test para las funciones que
+        crean dataframes, el programa
+        debe arrojar siempre dataframes
+        que contengan datos.'''
 
     df = users_df
     MENSAJE_ERROR = 'The dataframe cannot be empty'
 
     # Verifying empty assertions
     assert df.count() > 0, MENSAJE_ERROR
+
 
 # Stop the Spark session
 spark.stop()
